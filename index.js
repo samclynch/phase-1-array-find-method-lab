7c3090bd0bad1ce16362797e2e29f307ca393db9
Sam Lynch <samdawkins1998@gmail.com>
2021-11-26 11:09:14 -0500

    commit tree 0419b431442c1992ee4eb6dbb1ec761e948591a7

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const superbowlWin = (Array) => {
+    const obj = Array.find((record) => record.result === "W");
+    if (obj !== undefined) {
+        return obj.year;
+    } else {
+        return undefined;
+    }  
+};
